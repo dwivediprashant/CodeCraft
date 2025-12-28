c3d13b2fef2bba40aecd9e40b2d080bab86e5922
Apoorv Mittal <apoorvmitthu@gmail.com>
2025-12-28 17:50:00 +0530

    Apoorv Difference Easy

---
+++ algos/range_queries/DifferenceArray/soln/Apoorv012/Solution1.cpp
@@ -0,0 +1,9 @@
+/*
+Given an array of n integers, your task is to process q queries of the following types:
+- increase each value in range [a,b] by u
+- what is the value at position k?
+
+Time: 
+Space: 
+
+*/
